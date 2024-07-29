2cd0df614734a0a0821376749b42ddd2aeb7eb14
Sakthikannan Subramanian <40839233+SakthikannanSubramanian@users.noreply.github.com>
2024-07-29 10:39:50 +0100

    Removed Footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
     <footer className="footer">
-        <p>&copy; 2024 Website by our Fantastic JLP QE team</p>
+        <p>&copy; 2024 Website</p>
     </footer>
 );
 
